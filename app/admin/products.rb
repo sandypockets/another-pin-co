2f3dd965323787b3f8bc76447f24fec8f14a6c34
sandypockets <zac@znacreative.com>
2021-09-15 16:42:44 -0400

    Customize active_admin Product form

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,33 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :image_1, :image_2, :image_3, :image_4, :image_5, :price, :is_featured, :collection_date, :description, :is_sold_out
+  permit_params :title, :image_1, :image_2, :image_3, :image_4, :image_5, :price, :is_featured, :collection_date,
+                :description, :is_sold_out
+
+  form do |f|
+    f.inputs "Product info" do
+      f.input :title
+      f.input :price
+      f.input :description
+      f.input :collection_date
+    end
+
+    f.inputs "Filters" do
+      f.input :is_featured, label: "Featured on homepage?"
+      f.input :is_sold_out, label: "Sold out?"
+    end
+
+    f.inputs "Images" do
+      f.input :image_1
+      f.input :image_2
+      f.input :image_3
+      f.input :image_4
+      f.input :image_5
+    end
+
+    f.actions
+  end
+
   #
   # or
   #
@@ -14,5 +40,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
