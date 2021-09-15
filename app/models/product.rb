56cc0328023a066b44597c8af9958744c070ee29
sandypockets <zac@znacreative.com>
2021-09-15 16:16:25 -0400

    Add ProductImageUploader

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :description, presence: true
 
+  mount_uploader :image_1, ProductImageUploader
+  mount_uploader :image_2, ProductImageUploader
+  mount_uploader :image_3, ProductImageUploader
+  mount_uploader :image_4, ProductImageUploader
+  mount_uploader :image_5, ProductImageUploader
+
 end
