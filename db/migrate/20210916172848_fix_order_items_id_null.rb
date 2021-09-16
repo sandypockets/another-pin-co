348523b02a3124b3799f50367900e3cd8b0eba0e
sandypockets <zac@znacreative.com>
2021-09-16 13:44:30 -0400

    Run migration to fix default value of null for order_id

---
+++ db/migrate/20210916172848_fix_order_items_id_null.rb
@@ -0,0 +1,5 @@
+class FixOrderItemsIdNull < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :order_items, :order_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_144015) do
+ActiveRecord::Schema.define(version: 2021_09_16_172848) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_09_16_144015) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "order_id", null: false
+    t.integer "order_id"
     t.integer "product_id", null: false
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
