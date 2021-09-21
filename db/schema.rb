3a28936eba5203b566e7815fc109ca0ac7cd3eeb
sandypockets <zac@znacreative.com>
2021-09-21 11:38:03 -0400

    Add stripe_token migration for orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_172848) do
+ActiveRecord::Schema.define(version: 2021_09_21_153427) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_09_16_172848) do
     t.string "postal_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_token"
   end
 
   create_table "products", force: :cascade do |t|
